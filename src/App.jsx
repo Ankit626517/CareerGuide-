7b4f2f86c82ebc9668ea193e8e85d95a6e635e0b
Ankit Rathor <ankitrathore48132@gmail.com>
2025-06-27 22:48:54 +0530

    Fix import casing and formatting in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
-import Navbar from './component/Navbar'
-import Home from './component/Home'
+import Navbar from './Component/Navbar'
+import Home from './Component/Home'
 import ExploreCareer from './Component/Explore'
 
-import CareerTest from './component/CareerTest'
+import CareerTest from './Component/CareerTest'
 import Footer from './Pages/Footer'
 import Education from './Component/Educations'
 
@@ -14,21 +14,21 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <>
-  <BrowserRouter>
-    <Navbar />
-    <div className='pt-16'>    <Routes >
-      
-      <Route path="/" element={<Home />} />
-      <Route path="/explore-career" element={<ExploreCareer />} />
-      <Route path="/education" element={<Education />} />
-      <Route path="/career-test" element={<CareerTest />} />
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <div className='pt-16'>    <Routes >
 
-    </Routes>
-    </div>
-<Footer />
-  </BrowserRouter>
-   </>
+          <Route path="/" element={<Home />} />
+          <Route path="/explore-career" element={<ExploreCareer />} />
+          <Route path="/education" element={<Education />} />
+          <Route path="/career-test" element={<CareerTest />} />
+
+        </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </>
   )
 }
 
